922889033a50f479792905dd43f2024c5724aa3d
Vincent Tran <vtran6971@bths.edu>
2018-12-14 17:49:01 -0500

    Completed and commented

--- src/com/company/FaceDrawing.java
+++ src/com/company/FaceDrawing.java
@@ -15,14 +15,39 @@ public class FaceDrawing {
      faceFeatures[row][column] = replace;
     }
 
-    //?????????
+    //Fills every index in the array with a single string
     void fill(String str)
     {
+        String index = "";
+        int x = 0;
 
+        for (int i = 0; i < faceFeatures[0].length; i++) {
+            if (faceFeatures[x][i] == null) {
+                faceFeatures[x][i] = str;
+            }
+            faceFeatures[x][i] = str;
+
+            index = index + "" + faceFeatures[x][i];
+
+            if (i+1 == faceFeatures[0].length)
+            {
+                x += 1;
+                i = -1;
+                //ACTIVATION of this line and the NEXT will print the array line by line  System.out.println(index);
+                //index = "";
+            }
+
+            if (x == faceFeatures.length)
+            {
+                i = faceFeatures[0].length;
+            }
+        }
     }
 
+    //Logic --> runs through a row checking to see if any index isn't declared and in that case declares it as an empty space
     //Logic --> runs through a row adding all the strings together into one
-    //Logic --> when the end of a row is reached reset back to beginning of the next row
+    //Logic --> when the end of a row is reached reset back to beginning of the next row and prints out the previous line and emptys out the total string
+    //Logic --> continues until it reaches the last row and ends the for loop
     public String toString()
     {
         String index = "";
@@ -30,11 +55,24 @@ public class FaceDrawing {
 
         for (int i = 0; i < faceFeatures[0].length; i++)
         {
-            index += faceFeatures[x][i];
+            if ( faceFeatures[x][i] == null)
+            {
+                faceFeatures[x][i] = " ";
+            }
+
+            index = index + "" + faceFeatures[x][i];
+
             if (i+1 == faceFeatures[0].length)
             {
-                i = 0;
                 x += 1;
+                i = -1;
+                System.out.println(index);
+                index = "";
+            }
+
+            if (x == faceFeatures.length)
+            {
+                i = faceFeatures[0].length;
             }
         }
         return index;
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -2,27 +2,150 @@ package com.company;
 
 public class Runner {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 8;
+        int x = 8;
+        int y = 18;
 
         String[][] face = new String[x][y];
         FaceDrawing face1 = new FaceDrawing(face);
 
         face1.edit(" ", 0,0);
-        face1.edit(" ", 4, 7);
-        face1.edit(" ", 0,7);
-        face1.edit(" ", 4, 0);
+        face1.edit(" ", 0,1);
+        face1.edit(" ", 0,2);
+        face1.edit(" ", 0,3);
+        face1.edit(" ", 0,4);
+        face1.edit(" ", 0,5);
+        face1.edit(" ", 0,6);
+        face1.edit("_", 0,7);
+        face1.edit("_", 0,8);
+        face1.edit("_", 0,9);
+        face1.edit("_", 0,10);
+        face1.edit("_", 0,11);
+        face1.edit("_", 0,12);
+        face1.edit("_", 0,13);
+        face1.edit("_", 0,14);
+        face1.edit(" ", 0,15);
+        face1.edit("", 0,16);
+        face1.edit("", 0,17);
 
-        face1.edit("0", 1,2);
-        face1.edit("0", 1, 5);
-        face1.edit("^", 2,3);
-        face1.edit("^", 2, 4);
-        face1.edit("|", 3,1);
-        face1.edit("|", 3, 2);
-        face1.edit("_", 3,3);
-        face1.edit("_", 3, 4);
-        face1.edit("|", 3,5);
-        face1.edit("|", 3, 6);
-        System.out.println(face1);
+        face1.edit(" ", 1,0);
+        face1.edit(" ", 1,1);
+        face1.edit(" ", 1,2);
+        face1.edit(" ", 1,3);
+        face1.edit(" ", 1,4);
+        face1.edit(" ", 1,5);
+        face1.edit("/", 1,6);
+        face1.edit(" ", 1,7);
+        face1.edit("O", 1,8);
+        face1.edit(" ", 1,9);
+        face1.edit(" ", 1,10);
+        face1.edit(" ", 1,11);
+        face1.edit(" ", 1,12);
+        face1.edit("O", 1,13);
+        face1.edit(" ", 1,14);
+        face1.edit("|", 1,15);
+        face1.edit("", 1,16);
+        face1.edit("", 1,17);
+
+        face1.edit(" ", 2,0);
+        face1.edit(" ", 2,1);
+        face1.edit(" ", 2,2);
+        face1.edit(" ", 2,3);
+        face1.edit(" ", 2,4);
+        face1.edit("/", 2,5);
+        face1.edit(" ", 2,6);
+        face1.edit("_", 2,7);
+        face1.edit("_", 2,8);
+        face1.edit("_", 2,9);
+        face1.edit("_", 2,10);
+        face1.edit("_", 2,11);
+        face1.edit(" ", 2,12);
+        face1.edit(" ", 2,13);
+        face1.edit("|", 2,14);
+        face1.edit("", 2,15);
+        face1.edit("", 2,16);
+        face1.edit("", 2,17);
+
+        face1.edit(" ", 3,0);
+        face1.edit(" ", 3,1);
+        face1.edit(" ", 3,2);
+        face1.edit(" ", 3,3);
+        face1.edit("/", 3,4);
+        face1.edit("-", 3,5);
+        face1.edit(" ", 3,6);
+        face1.edit(" ", 3,7);
+        face1.edit(" ", 3,8);
+        face1.edit(" ", 3,9);
+        face1.edit(" ", 3,10);
+        face1.edit(" ", 3,11);
+        face1.edit("-", 3,12);
+        face1.edit("/", 3,13);
+        face1.edit("", 3,14);
+        face1.edit("", 3,15);
+        face1.edit("", 3,16);
+        face1.edit("", 3,17);
+
+        face1.edit(" ", 4,0);
+        face1.edit(" ", 4,1);
+        face1.edit(" ", 4,2);
+        face1.edit("/", 4,3);
+        face1.edit(" ", 4,4);
+        face1.edit(" ", 4,5);
+        face1.edit(" ", 4,6);
+        face1.edit(" ", 4,7);
+        face1.edit(" ", 4,8);
+        face1.edit(" ", 4,9);
+        face1.edit(" ", 4,10);
+        face1.edit("/", 4,11);
+        face1.edit(" ", 4,12);
+        face1.edit(" ", 4,13);
+
+        face1.edit(" ", 5,0);
+        face1.edit(" ", 5,1);
+        face1.edit("/", 5,2);
+        face1.edit(" ", 5,3);
+        face1.edit(" ", 5,4);
+        face1.edit(" ", 5,5);
+        face1.edit(" ", 5,6);
+        face1.edit(" ", 5,7);
+        face1.edit(" ", 5,8);
+        face1.edit(" ", 5,9);
+        face1.edit(" ", 5,10);
+        face1.edit("|", 5,11);
+        face1.edit(" ", 5,12);
+        face1.edit(" ", 5,13);
+
+        face1.edit(" ", 6,0);
+        face1.edit("/", 6,1);
+        face1.edit(" ", 6,2);
+        face1.edit(" ", 6,3);
+        face1.edit(" ", 6,4);
+        face1.edit(" ", 6,5);
+        face1.edit(" ", 6,6);
+        face1.edit(" ", 6,7);
+        face1.edit(" ", 6,8);
+        face1.edit(" ", 6,9);
+        face1.edit(" ", 6,10);
+        face1.edit("|", 6,11);
+        face1.edit(" ", 6,12);
+        face1.edit(" ", 6,13);
+
+        face1.edit("/", 7,0);
+        face1.edit(" ", 7,1);
+        face1.edit(" ", 7,2);
+        face1.edit(" ", 7,3);
+        face1.edit(" ", 7,4);
+        face1.edit(" ", 7,5);
+        face1.edit(" ", 7,6);
+        face1.edit(" ", 7,7);
+        face1.edit(" ", 7,8);
+        face1.edit(" ", 7,9);
+        face1.edit(" ", 7,10);
+        face1.edit("|", 7,11);
+        face1.edit(" ", 7,12);
+        face1.edit(" ", 7,13);
+
+        face1.toString();
+        face1.fill(":>");
+
     }
 }

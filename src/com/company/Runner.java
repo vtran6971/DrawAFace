3ba5b1a76189676f61941ed9babab94925ed9666
Vincent Tran <vtran6971@bths.edu>
2018-12-14 00:36:23 -0500

    minor changes

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -2,15 +2,27 @@ package com.company;
 
 public class Runner {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 2;
+        int x = 5;
+        int y = 8;
 
         String[][] face = new String[x][y];
         FaceDrawing face1 = new FaceDrawing(face);
 
-        System.out.println(face.length);
-        System.out.println(face[0].length);
+        face1.edit(" ", 0,0);
+        face1.edit(" ", 4, 7);
+        face1.edit(" ", 0,7);
+        face1.edit(" ", 4, 0);
 
-
+        face1.edit("0", 1,2);
+        face1.edit("0", 1, 5);
+        face1.edit("^", 2,3);
+        face1.edit("^", 2, 4);
+        face1.edit("|", 3,1);
+        face1.edit("|", 3, 2);
+        face1.edit("_", 3,3);
+        face1.edit("_", 3, 4);
+        face1.edit("|", 3,5);
+        face1.edit("|", 3, 6);
+        System.out.println(face1);
     }
 }
